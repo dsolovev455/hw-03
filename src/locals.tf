bb8ba0152beda71bcbe6e86145892ed597138e2a
Bushuev A.V. DevOps-23 <sasha047@mail.ru>
2023-10-16 16:50:11 +0700

    edit outputs.ts and locals.tf

--- src/locals.tf
+++ src/locals.tf
@@ -1,6 +1,13 @@
+#locals{
+#    vms_metadata = {
+#      serial-port-enable = 1
+#      ssh-keys  = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIk1IXFg5e0XiyXLmnlsdaynRlCK4b3bY+slqvmiDQlE sasha047@mail.ru"
+#    }
+#}
+
 locals{
     vms_metadata = {
       serial-port-enable = 1
-      ssh-keys  = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIk1IXFg5e0XiyXLmnlsdaynRlCK4b3bY+slqvmiDQlE sasha047@mail.ru"
+      ssh-keys  = "ubuntu:${file("~/.ssh/id_ed25519.pub")} " 
     }
 }
--- src/outputs.tf
+++ src/outputs.tf
@@ -1,39 +1,70 @@
+## all vm ##
+
+output "instances" {
+  description = "Information about the instances"
+  value = {
+    web = [
+      for instance in yandex_compute_instance.web : {
+        name = instance.name
+        id   = instance.id
+        fqdn = instance.fqdn
+      }
+    ],
+    db = {
+      for vm in local.vms_bav : vm["vm_name"] => {
+        name  = yandex_compute_instance.db[vm["vm_name"]]["name"]
+        fqdn  = yandex_compute_instance.db[vm["vm_name"]]["fqdn"]
+        id    = yandex_compute_instance.db[vm["vm_name"]]["id"]
+      }
+    },
+    storage = {
+      name = yandex_compute_instance.storage[*].name
+      id   = yandex_compute_instance.storage[*].id
+      fqdn = yandex_compute_instance.storage[*].fqdn
+    }
+  }
+}
+
+
+
+
+
 ## web ##
 
-output "web" {
-  description = "Information about the instance web"
-  value       = [
-    for instance in yandex_compute_instance.web: {
-    name = instance.name
-    id   = instance.id
-    fqdn = instance.fqdn
-    }
-  ]
-}
+#output "web" {
+#  description = "Information about the instance web"
+#  value       = [
+#    for instance in yandex_compute_instance.web: {
+#    name = instance.name
+#    id   = instance.id
+#    fqdn = instance.fqdn
+#    }
+#  ]
+#}
 
 ## db ##
 
-output "db" {
-  description = "Information about the instance db"
-  value = {
-    for vm in local.vms_bav : vm["vm_name"] => {
-      name  = yandex_compute_instance.db[vm["vm_name"]]["name"]
-      fqdn  = yandex_compute_instance.db[vm["vm_name"]]["fqdn"]
-      id    = yandex_compute_instance.db[vm["vm_name"]]["id"]
-    }
-  }
-}
+#output "db" {
+#  description = "Information about the instance db"
+#  value = {
+#    for vm in local.vms_bav : vm["vm_name"] => {
+#      name  = yandex_compute_instance.db[vm["vm_name"]]["name"]
+#      fqdn  = yandex_compute_instance.db[vm["vm_name"]]["fqdn"]
+#      id    = yandex_compute_instance.db[vm["vm_name"]]["id"]
+#    }
+#  }
+#}
 
 ## storage ##
 
-output "storage" {
-  description = "Information about the instance storage"
-  value = {
-    name = yandex_compute_instance.storage[*].name
-    id   = yandex_compute_instance.storage[*].id
-    fqdn = yandex_compute_instance.storage[*].fqdn
-  }
-}
+#output "storage" {
+#  description = "Information about the instance storage"
+#  value = {
+#    name = yandex_compute_instance.storage[*].name
+#    id   = yandex_compute_instance.storage[*].id
+#    fqdn = yandex_compute_instance.storage[*].fqdn
+#  }
+#}
 
 ### VM WEB ###
 #output "web_name" {
